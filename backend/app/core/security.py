55a892a38a3dc134fe83e008470df1438b48279c
Hadi99999437 <abdulhadiawan012@gmail.com>
2026-07-27 21:40:33 +0500

    feat: add decode_access_token function for JWT validation and create db session management

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta, UTC
 from jose import jwt
 from passlib.context import CryptContext
 from app.core.config import settings
+from jose import JWTError
+
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
@@ -34,3 +36,20 @@ def create_access_token(data: dict):
         SECRET_KEY,
         algorithm=ALGORITHM,
     )
+
+
+def decode_access_token(token: str):
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM],
+        )
+
+        return payload
+
+    except JWTError:
+
+        return None    
---
+++ backend/dependancies/db.py
@@ -0,0 +1,8 @@
+from app.db.session import AsyncSessionLocal
+
+
+async def get_db():
+
+    async with AsyncSessionLocal() as session:
+
+        yield session

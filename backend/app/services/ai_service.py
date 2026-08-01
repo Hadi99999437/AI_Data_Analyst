a82994ce3b56c07cd5b1fa62400adf36f7b377ba
Hadi99999437 <abdulhadiawan012@gmail.com>
2026-08-01 16:20:35 +0500

    feat: implement generate_analysis method in AIService for generating professional reports from dataset summaries

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -1,5 +1,63 @@
+import json
+import google.generativeai as genai
+
+from app.core.config import settings
+
+
 class AIService:
 
-    async def explain_dataset(self, analysis_result):
+    def __init__(self):
 
-        pass
+        genai.configure(
+            api_key=settings.GEMINI_API_KEY
+        )
+
+        self.model = genai.GenerativeModel(
+            "gemini-2.5-flash"
+        )
+
+    async def generate_analysis(
+        self,
+        analysis_result: dict,
+    ):
+
+        prompt = f"""
+You are an expert Senior Data Analyst.
+
+Below is the statistical summary of a dataset.
+
+{json.dumps(analysis_result, indent=2)}
+
+Generate a professional report.
+
+Return ONLY valid JSON.
+
+Format:
+
+{{
+    "summary":"...",
+    "insights":[
+        "...",
+        "...",
+        "..."
+    ],
+    "recommendations":[
+        "...",
+        "...",
+        "..."
+    ]
+}}
+"""
+
+        response = self.model.generate_content(
+            prompt
+        )
+
+        text = response.text.strip()
+
+        if text.startswith("```json"):
+            text = text.replace("```json", "")
+            text = text.replace("```", "")
+            text = text.strip()
+
+        return json.loads(text)

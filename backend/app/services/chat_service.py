7ee468be29cc6ff96e6111c183ce263fdd12ff79
Hadi99999437 <abdulhadiawan012@gmail.com>
2026-08-04 13:53:38 +0500

    feat: implement ChatService to generate professional responses based on dataset analysis and user questions

--- backend/app/services/chat_service.py
+++ backend/app/services/chat_service.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
+from app.services.ai_service import AIService
+
 
 class ChatService:
 
     def __init__(self, dataset_repo):
+
         self.dataset_repo = dataset_repo
+
+        self.ai_service = AIService()
 
     async def ask_question(
         self,
@@ -23,4 +28,37 @@ class ChatService:
             dataset.storage_path
         )
 
-        return df
+        prompt = f"""
+You are an expert Data Analyst.
+
+Dataset Information
+
+Rows:
+{len(df)}
+
+Columns:
+{df.columns.tolist()}
+
+Data Types:
+{df.dtypes.astype(str).to_dict()}
+
+Missing Values:
+{df.isnull().sum().to_dict()}
+
+Summary Statistics:
+{df.describe(include='all').fillna('').to_string()}
+
+Sample Data:
+{df.head(20).to_markdown(index=False)}
+
+User Question:
+{question}
+
+Answer professionally in few sentences .
+"""
+
+        answer = await self.ai_service.generate_chat_response(
+            prompt
+        )
+
+        return answer

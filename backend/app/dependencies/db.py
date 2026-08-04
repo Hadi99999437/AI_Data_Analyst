4aed365ecaae63ffc9f1451a2f80573045c0c21a
Hadi99999437 <abdulhadiawan012@gmail.com>
2026-08-04 15:24:25 +0500

    feat: refactor report retrieval by moving logic to report.py and updating database dependency functions

--- backend/app/dependencies/db.py
+++ backend/app/dependencies/db.py
@@ -1,8 +1,32 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.db.session import AsyncSessionLocal
+
+from app.repositories.dataset_repository import DatasetRepository
+from app.repositories.user_repository import UserRepository
+from app.repositories.analysis_repository import AnalysisRepository
 
 
 async def get_db():
 
     async with AsyncSessionLocal() as session:
-
         yield session
+
+
+async def get_dataset_repository(
+    db: AsyncSession = Depends(get_db),
+):
+    return DatasetRepository(db)
+
+
+async def get_user_repository(
+    db: AsyncSession = Depends(get_db),
+):
+    return UserRepository(db)
+
+
+async def get_analysis_repository(
+    db: AsyncSession = Depends(get_db),
+):
+    return AnalysisRepository(db)

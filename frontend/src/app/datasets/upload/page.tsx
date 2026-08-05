dd2d9fa924d48fad9b5d739c84e9935743c79f00
Hadi99999437 <abdulhadiawan012@gmail.com>
2026-08-05 14:26:07 +0500

    feat: add UploadPage component for dataset uploads

---
+++ frontend/src/app/datasets/upload/page.tsx
@@ -0,0 +1,9 @@
+import UploadDataset from "@/components/dataset/UploadDataset";
+
+export default function UploadPage() {
+    return (
+        <div className="p-8">
+            <UploadDataset />
+        </div>
+    );
+}
